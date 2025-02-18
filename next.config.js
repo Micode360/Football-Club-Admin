f0bfa8f2dc4f35136beffa641442bf3b0f202b11
unknown <abayemiracle@gmail.com>
2025-02-18 21:37:16 +0100

    added removing of console.log to next.congig

--- next.config.js
+++ next.config.js
@@ -8,6 +8,17 @@ const nextConfig = {
     unoptimized: true,
     domains: ["flagcdn.com", "upload.wikimedia.org", "res.cloudinary.com"],
   },
+  webpack: (config, { isServer, dev }) => {
+    if (!dev && !isServer) {
+      // Remove console.log, console.warn, and console.error in production
+      config.optimization.minimizer.forEach((minimizer) => {
+        if (minimizer.constructor.name === 'TerserPlugin') {
+          minimizer.options.terserOptions.compress.drop_console = true;
+        }
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
