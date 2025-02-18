6cf20ba0b20dfbd86b6f27e2184d2d5e636965cd
unknown <abayemiracle@gmail.com>
2025-02-18 21:25:58 +0100

    taken out the log

--- next.config.js
+++ next.config.js
@@ -8,37 +8,6 @@ const nextConfig = {
     unoptimized: true,
     domains: ["flagcdn.com", "upload.wikimedia.org", "res.cloudinary.com"],
   },
-  webpack: (config, { dev }) => {
-    if (!dev) {
-      // Remove console logs in production
-      config.optimization.minimizer.push({
-        apply: (compiler) => {
-          compiler.hooks.compilation.tap("RemoveConsole", (compilation) => {
-            compilation.hooks.processAssets.tap(
-              {
-                name: "RemoveConsole",
-                stage: compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_SIZE,
-              },
-              (assets) => {
-                for (const assetName in assets) {
-                  if (assetName.endsWith(".js")) {
-                    assets[assetName] = {
-                      source: () =>
-                        assets[assetName]
-                          .source()
-                          .replace(/console\.(log|warn|error|debug)\(.*?\);?/g, ""),
-                      size: () => assets[assetName].source().length,
-                    };
-                  }
-                }
-              }
-            );
-          });
-        },
-      });
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
